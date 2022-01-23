5cc8f1a809fd5b0eaf32f0a5b27bc0cde959e1a4
문몰리 <gsgslolo@naver.com>
2022-01-23 21:57:41 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const Express =require ("express");
+const app = Express();
+
+app.get('/',(req,res)=>{
+    res.send('init page')
+});
+
+app.listen(3000,()=>{
+    console.log('3000번 포트에 server를 띄웠습니다.')
+})
