d8a9203d820d19e983c08035bb93d03bd115c30f
문몰리 <gsgslolo@naver.com>
2022-01-26 00:44:04 +0900

    :construction: init route
    init main route

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const Express =require ("express");
+const routes = require('./routes');
+const path = require('path');
 const app = Express();
 
-app.get('/',(req,res)=>{
-    res.send('init page')
-});
+
+app.use('/', routes);
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views')); 
 
 app.listen(3000,()=>{
     console.log('3000번 포트에 server를 띄웠습니다.')
 })
- 
+
