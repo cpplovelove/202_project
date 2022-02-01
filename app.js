f336d9526382d0affaa321176a3253193bf0acee
문몰리 <gsgslolo@naver.com>
2022-02-01 22:40:55 +0900

    edit heroku init

--- app.js
+++ app.js
@@ -9,11 +9,10 @@ app.use('/', routes);
 
 app.use('/', Express.static(path.join(__dirname, './public')));  
 
-
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views')); 
 
-app.listen(3000,()=>{
+app.listen(process.env.PORT || 3000,()=>{
     console.log('3000번 포트에 server를 띄웠습니다.')
 })
 
